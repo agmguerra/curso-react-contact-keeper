120a74eca3bb679a38d660668eedf89571ac69fb
AlexandreGuerra <agmguerra@gmail.com>
2020-04-12 22:10:53 -0300

    ajuste no server.js para retirar a rota para uma mensagem basica

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send({ msg: 'Welcome to the ContactKeeper API...'}));
+//app.get('/', (req, res) => res.send({ msg: 'Welcome to the ContactKeeper API...'}));
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
